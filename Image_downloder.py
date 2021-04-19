b934fa0c0a105e8c2787822d07c127c1953da2ec
flsbnus <81967132+flsbnus@users.noreply.github.com>
2021-04-19 21:57:03 +0900

    깃허브크롤링

---
+++ Image_downloder.py
@@ -0,0 +1,20 @@
+from urllib.request import Request,urlopen
+import json
+
+api_url='https://api.github.com/repos/prajnasb/observations/contents/experiements/data/without_mask?ref=master'
+
+hds={'User-Agent':'Mozilla/5.0'}
+
+request = Request(api_url,headers=hds)
+response = urlopen(request)
+directory_bytes = response.read()
+print(type(directory_bytes))
+directory_str=directory_bytes.decode('utf-8')
+print(type(directory_str))
+
+contents=json.loads(directory_str)
+
+for i in range(len(contents)):
+    content=contents[i]
+    print(content['download_url'])
+    break
