e9b65d6cd277a7165d931812df56ee86dddcee45
flsbnus <81967132+flsbnus@users.noreply.github.com>
2021-04-28 21:56:36 +0900

    마스크 위치 설정

--- Image_downloader.py
+++ Image_downloader.py
@@ -2,6 +2,23 @@ from urllib.request import Request,urlopen
 import json
 import os
 
+def image_download(url,filepath):
+    request=Request(url)
+    response=urlopen(request)
+
+    image_data=response.read()
+
+    file=open(filepath,'wb')
+    file.write(image_data)
+    file.close()
+    print(url+'로 부터'+filepath+'에 다운로드 완료')
+    #url로 부터 filepath에 다운로드 완료
+
+mask_url='https://github.com/prajnasb/observations/raw/master/mask_classifier/Data_Generator/images/blue-mask.png'
+image_download(mask_url,'data/mask.png')
+exit()
+
+
 api_url='https://api.github.com/repos/prajnasb/observations/contents/experiements/data/without_mask?ref=master'
 
 hds={'User-Agent':'Mozilla/5.0'}
@@ -30,5 +47,6 @@ for i in range(len(contents)):
     image_file=open('data/without_mask/'+content['name'],'wb')
     image_file.write(image_data)
     print('다운로드 완료('+str(i+1)+'/'+str(len(contents))+'): '+ content['name'])
+    break
 
 
